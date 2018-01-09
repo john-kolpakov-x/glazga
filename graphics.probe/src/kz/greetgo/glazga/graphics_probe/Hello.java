d587f6dd52adeeee8df5ad08dad2a28bdc8e4948
ekolpakov <ekolpakov@greet-go.com>
2018-01-09 13:36:39 +0600

    Добавлен пустой класс

--- graphics.probe/src/kz/greetgo/glazga/graphics_probe/Hello.java
+++ graphics.probe/src/kz/greetgo/glazga/graphics_probe/Hello.java
@@ -2,6 +2,6 @@ package kz.greetgo.glazga.graphics_probe;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    System.out.println("Привет!");
   }
 }
