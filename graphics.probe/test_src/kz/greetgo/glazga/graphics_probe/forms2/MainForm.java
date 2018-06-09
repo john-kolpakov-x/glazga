988930c1338ad8362d31c2288c25bb705b75e98b
ekolpakov <ekolpakov@greet-go.com>
2018-06-10 00:13:17 +0600

    Улучшена прорисовка

--- graphics.probe/test_src/kz/greetgo/glazga/graphics_probe/forms2/MainForm.java
+++ graphics.probe/test_src/kz/greetgo/glazga/graphics_probe/forms2/MainForm.java
@@ -4,6 +4,7 @@ import kz.greetgo.glazga.graphics_probe.fonts.Fonts;
 
 import javax.swing.SwingUtilities;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics2D;
 import java.awt.event.ComponentAdapter;
@@ -11,6 +12,7 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -83,17 +85,40 @@ public class MainForm {
 
       paintPanel.startPaint();
 
+      BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+      image.getType();
+
       while (working.get()) {
 
-        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-        Fonts.get().applyHints(g);
-        try {
-          paintPanel.paint(g, canvas.getWidth(), canvas.getHeight());
-        } finally {
-          g.dispose();
-        }
-        bufferStrategy.show();
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+        if (width > 1 && height > 1) {
 
+          if (image.getWidth() != width || image.getHeight() != height) {
+            image = new BufferedImage(width, height, image.getType());
+            System.out.println("Changed image size to " + width + "x" + height);
+          }
+
+          {
+            Graphics2D g = image.createGraphics();
+            Fonts.get().applyHints(g);
+            g.setColor(new Color(180, 180, 180));
+            g.fillRect(0, 0, width, height);
+            try {
+              paintPanel.paint(g, canvas.getWidth(), canvas.getHeight());
+            } finally {
+              g.dispose();
+            }
+          }
+
+          {
+            Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+            g.drawImage(image, 0, 0, null);
+            g.dispose();
+          }
+          bufferStrategy.show();
+
+        }
         try {
           Thread.sleep(1000 / 24);
         } catch (InterruptedException ignore) {}
