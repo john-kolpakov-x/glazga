c5527e2ba6b732c350cb7c89c00cb4542270ee48
ekolpakov <ekolpakov@greet-go.com>
2019-06-27 20:11:35 +0600

    Base LWJGL window show

--- lwjgl.probe/src/kz/glazga/lwjgl/probe/HelloWorld.java
+++ lwjgl.probe/src/kz/glazga/lwjgl/probe/HelloWorld.java
@@ -1,45 +1,25 @@
-package kz.glazga.jogl.probe;
+package kz.glazga.lwjgl.probe;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.IntBuffer;
 
 import static java.util.Objects.requireNonNull;
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
 import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
 import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
-import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class HelloWorld {
@@ -54,12 +34,12 @@ public class HelloWorld {
     loop();
 
     // Free the window callbacks and destroy the window
-    glfwFreeCallbacks(window);
-    glfwDestroyWindow(window);
+    Callbacks.glfwFreeCallbacks(window);
+    GLFW.glfwDestroyWindow(window);
 
     // Terminate GLFW and free the error callback
-    glfwTerminate();
-    GLFWErrorCallback glfwErrorCallback = glfwSetErrorCallback(null);
+    GLFW.glfwTerminate();
+    GLFWErrorCallback glfwErrorCallback = GLFW.glfwSetErrorCallback(null);
     if (glfwErrorCallback != null) {
       glfwErrorCallback.free();
     }
@@ -71,52 +51,57 @@ public class HelloWorld {
     GLFWErrorCallback.createPrint(System.err).set();
 
     // Initialize GLFW. Most GLFW functions will not work before doing this.
-    if (!glfwInit())
+    if (!GLFW.glfwInit()) {
       throw new IllegalStateException("Unable to initialize GLFW");
+    }
 
     // Configure GLFW
-    glfwDefaultWindowHints(); // optional, the current window hints are already the default
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+    GLFW.glfwDefaultWindowHints(); // optional, the current window hints are already the default
+    GLFW.glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+    GLFW.glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
     // Create the window
-    window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-    if (window == NULL)
+    window = GLFW.glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+    if (window == NULL) {
       throw new RuntimeException("Failed to create the GLFW window");
+    }
 
     // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-    glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-      if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-        glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+    GLFW.glfwSetKeyCallback(window, (window, key, scanCode, action, mods) -> {
+      if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+        GLFW.glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+      }
     });
 
     // Get the thread stack and push a new frame
-    try (MemoryStack stack = stackPush()) {
+    try (MemoryStack stack = MemoryStack.stackPush()) {
       IntBuffer pWidth = stack.mallocInt(1); // int*
       IntBuffer pHeight = stack.mallocInt(1); // int*
 
       // Get the window size passed to glfwCreateWindow
-      glfwGetWindowSize(window, pWidth, pHeight);
+      GLFW.glfwGetWindowSize(window, pWidth, pHeight);
+
+      long monitor = GLFW.glfwGetPrimaryMonitor();
 
       // Get the resolution of the primary monitor
-      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-      requireNonNull(vidmode);
+      GLFWVidMode vidMode = GLFW.glfwGetVideoMode(monitor);
+      requireNonNull(vidMode);
 
       // Center the window
-      glfwSetWindowPos(
+      GLFW.glfwSetWindowPos(
         window,
-        (vidmode.width() - pWidth.get(0)) / 2,
-        (vidmode.height() - pHeight.get(0)) / 2
+        (vidMode.width() - pWidth.get(0)) / 2,
+        (vidMode.height() - pHeight.get(0)) / 2
       );
     } // the stack frame is popped automatically
 
     // Make the OpenGL context current
-    glfwMakeContextCurrent(window);
+    GLFW.glfwMakeContextCurrent(window);
     // Enable v-sync
-    glfwSwapInterval(1);
+    GLFW.glfwSwapInterval(1);
 
     // Make the window visible
-    glfwShowWindow(window);
+    GLFW.glfwShowWindow(window);
   }
 
   private void loop() {
@@ -128,18 +113,18 @@ public class HelloWorld {
     GL.createCapabilities();
 
     // Set the clear color
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    GL11.glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
 
     // Run the rendering loop until the user has attempted to close
     // the window or has pressed the ESCAPE key.
-    while (!glfwWindowShouldClose(window)) {
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+    while (!GLFW.glfwWindowShouldClose(window)) {
+      GL11.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-      glfwSwapBuffers(window); // swap the color buffers
+      GLFW.glfwSwapBuffers(window); // swap the color buffers
 
       // Poll for window events. The key callback above will only be
       // invoked during this call.
-      glfwPollEvents();
+      GLFW.glfwPollEvents();
     }
   }
 
